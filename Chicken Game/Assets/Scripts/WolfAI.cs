c6c365711e31ca572dab7a2704351ba07572af79
Unknown <bethanylegge19@gmail.com>
2017-10-23 20:11:57 -0600

    added wolf AI and secondary chicken flee

---
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+
+
+	public float moveSpeed;
+	public Transform player;
+	public Transform chicken;
+	
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Playa"){
+			Debug.Log("Player is in view of the wolf");
+			transform.LookAt(player);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+		else if(other.gameObject.name == "McChicken"){
+			Debug.Log("Chicken is in view of the wolf");
+			transform.LookAt(chicken);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+		}
+		}
+	
+
