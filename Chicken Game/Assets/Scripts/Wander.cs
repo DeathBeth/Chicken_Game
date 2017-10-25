d5647e1110c9a662a4227f3dc44b8436945714ee
Unknown <bethanylegge19@gmail.com>
2017-10-25 00:00:49 -0600

    added useless scripts

---
+++ Chicken Game/Assets/Scripts/Wander.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+    public float duration;    
+    public float elapsedTime   = 0f; 
+    public float wait          = 0f; 
+    public float waitTime      = 0f; 
+
+    float randomX;  
+    float randomZ;  
+
+    bool move = true; 
+
+    void Start(){
+        randomX =  Random.Range(-3,3);
+        randomZ = Random.Range(-3,3);
+    }
+
+    void Update ()
+    {
+
+
+
+        
+
+        if (elapsedTime < duration && move) {
+            
+            transform.Translate (new Vector3(randomX,0,randomZ) * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+
+        } else {
+            
+            move        = false;
+            wait        = Random.Range (5, 10);
+            waitTime    = 0f;
+        }
+
+        if (waitTime < wait && !move) {
+            
+            waitTime += Time.deltaTime;
+
+} 
+	else if(!move) {
+            move = true;
+			elapsedTime = 0f;
+            randomX = Random.Range(-3,3);
+            randomZ = Random.Range(-3,3);
+        }
+    }
+
+
+
+
+}
---
+++ Chicken Game/Assets/Scripts/Wander5.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander5 : MonoBehaviour {
+
+
+	public enum MovementStates{
+		Idle,
+		Walking,
+		Resting
+	}
+
+	public MovementStates MovementType;
+
+	private float speed = 2.0f;
+	float rotateSpeed = 2.0f;
+	
+	public GameObject Waypoint;
+	GameObject WP;
+	bool wayP = false;
+
+	float min_distance = -2.0f;
+	float max_distance = 2.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		createWayPoint ();
+
+		if(MovementType == MovementStates.Walking){
+			createWayPoint ();
+		}
+		if(MovementType == MovementStates.Idle){
+
+		}
+
+		if(wayP){
+			move();
+		}
+	}
+
+	void move()
+	{
+		transform.position = Vector3.MoveTowards(transform.position,WP.transform.position, speed * Time.deltaTime);
+	}
+
+	void createWayPoint ()
+	{
+		if(!wayP)
+		{
+		float distance_x = transform.position.x + Random.Range(min_distance,max_distance);
+		float distance_z = transform.position.z + Random.Range(min_distance,max_distance);
+
+		WP = Instantiate(Waypoint,new Vector3(distance_x,0,distance_z),Quaternion.identity) as GameObject;
+
+
+		wayP = true;
+		}
+	}
+}
