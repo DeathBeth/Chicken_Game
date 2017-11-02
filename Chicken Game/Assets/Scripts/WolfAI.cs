0b65c5ca30591f4ad3c06357bdc19c373b9d5981
Unknown <bethanylegge19@gmail.com>
2017-11-01 20:15:21 -0600

    added player health and played with wolfai

--- Chicken Game/Assets/Scripts/WolfAI.cs
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -2,37 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+// [RequireComponent(typeof(CharacterController))]
 
 public class WolfAI : MonoBehaviour {
 
 	public Transform player;
 	public Transform chicken;
+	public int damage;
 
-	public Transform Wall;
+	public 
+
+	// public Transform Wall;
 
 	public float speed = 5;
-	public float directionChangeInterval = 15;
-	public float maxHeadingChange = 50;
+	// public float directionChangeInterval = 15;
+	// public float maxHeadingChange = 50;
 
-	CharacterController controller;
-	float heading;
-	Vector3 targetRotation;
+	// CharacterController controller;
+	// float heading;
+	// Vector3 targetRotation;
 
-	bool shouldMove = true;	
-	
-	// void OnTriggerTurn(Collider other){
-	// 	if(other.gameObject.name == "Wall"){
-	// 		transform.Translate(Vector3.)
-	// 	}
-	// }
-	
-	void OnCollisionEnter(Collision collision) {
-   if (collision.gameObject.name == "Wall") {            
-       Debug.Log ("Hit Wall!");
-       shouldMove = false;   
-   }
-}
+
 	void OnTriggerStay(Collider other){
 
 		if(other.gameObject.name == "Playa"){
@@ -46,16 +36,48 @@ public class WolfAI : MonoBehaviour {
 			transform.Translate(Vector3.forward*speed*Time.deltaTime);
 		}
 		}
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.name == "Playa"){
+			PlayerHealth.TakeDamage(damage);
+		}
 
-			void Awake ()
-{
-		controller = GetComponent<CharacterController>();
+		print("Wolf is attacking!");
+		var hit = other.gameObject;
+		var health = hit.GetComponent<PlayerHealth>();
 
-		heading = Random.Range(0, 360);
-		transform.eulerAngles = new Vector3(0, heading, 0);
+			if(health != null){
+				health.TakeDamage(damage);
+			}
+	}
+
+	
+
+	// bool shouldMove = true;	
+	
+	// void OnTriggerTurn(Collider other){
+	// 	if(other.gameObject.name == "Wall"){
+	// 		transform.Translate(Vector3.)
+	// 	}
+	// }
+	
+// 	void OnCollisionEnter(Collision collision) {
+//    if (collision.gameObject.name == "Wall") {            
+//        Debug.Log ("Hit Wall!");
+//        shouldMove = false;   
+//    }
+// }
+
+// 	void Awake ()
+// {
+// 		controller = GetComponent<CharacterController>();
+
+// 		heading = Random.Range(0, 360);
+// 		transform.eulerAngles = new Vector3(0, heading, 0);
 		
-		StartCoroutine(NewHeading());
-}	
+// 		StartCoroutine(NewHeading());
+// }	
+	
 
 	void Start () {
 		
@@ -64,27 +86,28 @@ public class WolfAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
-		var forward = transform.TransformDirection(Vector3.forward);
-		controller.SimpleMove(forward * speed);
-	}
+	// 	transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+	// 	var forward = transform.TransformDirection(Vector3.forward);
+	// 	controller.SimpleMove(forward * speed);
+	// }
 
-	IEnumerator NewHeading ()
-	{
-		while (true) {
-			NewHeadingRoutine();
-			yield return new WaitForSeconds(directionChangeInterval);
-		}
-	}
+	// IEnumerator NewHeading ()
+	// {
+	// 	while (true) {
+	// 		NewHeadingRoutine();
+	// 		yield return new WaitForSeconds(directionChangeInterval);
+	// 	}
+	// }
 
-	void NewHeadingRoutine ()
-		{
-			var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
-			var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
-			heading = Random.Range(floor, ceil);
-			targetRotation = new Vector3(0, heading, 0);
-		}
+	// void NewHeadingRoutine ()
+	// 	{
+	// 		var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+	// 		var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+	// 		heading = Random.Range(floor, ceil);
+	// 		targetRotation = new Vector3(0, heading, 0);
+	// 	}
 	
+}
 }
 
 
