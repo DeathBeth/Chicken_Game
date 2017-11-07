70bd895f39ab9c85ac0aed45ddcebceb3068b713
Unknown <bethanylegge19@gmail.com>
2017-11-06 20:02:11 -0700

    added generic script to look at

---
+++ Chicken Game/Assets/Scripts/Generic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic : MonoBehaviour {
+
+	//Position
+	// public float posX;
+	// public float posY;
+	// public float posZ;
+	public Vector3 pos;
+	//Rotate
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
+	// public float rotW;
+	public Quaternion rot;
+
+	//Scale
+	// public float x = 1.0f;
+	// public float y = 1.0f;
+	// public float z = 1.0f;
+	public Vector3 scale;
+	void Start () {
+		scale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Position
+		// transform.position = new Vector3(posX, posY, posZ);
+
+		// //Rotation
+		// transform.rotation = new Quaternion(rotX, rotY, rotZ, rotW);
+
+		// //Scale
+		// transform.localScale = new Vector3(x,y,z);
+
+		transform.position = pos;
+		transform.rotation = rot;
+		scale = Vector3.one;
+	}
+}
--- Chicken Game/Assets/Scripts/WolfAI.cs
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -8,7 +8,10 @@ public class WolfAI : MonoBehaviour {
 
 	public Transform player;
 	public Transform chicken;
-	// public int damage;
+	public int damage;
+
+
+
 
 	// public Transform Wall;
 
@@ -16,8 +19,7 @@ public class WolfAI : MonoBehaviour {
 	public float rotSpeed = 10.0f;
 	Vector3 turnAround = new Vector3(0,1,0);
 
-	// public float directionChangeInterval = 15;
-	// public float maxHeadingChange = 50;
+	
 
 	// CharacterController controller;
 	// float heading;
@@ -25,6 +27,7 @@ public class WolfAI : MonoBehaviour {
 
 
 	// void OnTriggerStay(Collider other){
+
 
 	// 	if(other.gameObject.name == "Playa"){
 	// 		Debug.Log("Player is in view of the wolf");
@@ -40,7 +43,25 @@ public class WolfAI : MonoBehaviour {
 
 	void Update (){
 			Wander ();
+		
+		
+		// if(other.gameObject.name == "Playa"){
+		// 	Debug.Log("Player is in view of the wolf");
+		// 	transform.LookAt(player);
+		// 	transform.Translate(Vector3.forward*speed*Time.deltaTime);
+		// }
+		// else if(other.gameObject.name == "McChicken"){
+		// 	Debug.Log("Chicken is in view of the wolf");
+		// 	transform.LookAt(chicken);
+		// 	transform.Translate(Vector3.forward*speed*Time.deltaTime);
+		// }
 		}
+	void OnCollisionEnter(Collision other)
+	{
+		// if(other.gameObject.name == "Playa"){
+		// 	PlayerHealth.TakeDamage(damage);
+		// 
+	}
 
 	void Wander (){
 		transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -66,62 +87,7 @@ public class WolfAI : MonoBehaviour {
 	// 		}
 	// }
 
-	
 
-	// bool shouldMove = true;	
-	
-	// void OnTriggerTurn(Collider other){
-	// 	if(other.gameObject.name == "Wall"){
-	// 		transform.Translate(Vector3.)
-	// 	}
-	// }
-	
-// 	void OnCollisionEnter(Collision collision) {
-//    if (collision.gameObject.name == "Wall") {            
-//        Debug.Log ("Hit Wall!");
-//        shouldMove = false;   
-//    }
-// }
-
-// 	void Awake ()
-// {
-// 		controller = GetComponent<CharacterController>();
-
-// 		heading = Random.Range(0, 360);
-// 		transform.eulerAngles = new Vector3(0, heading, 0);
-		
-// 		StartCoroutine(NewHeading());
-// }	
-	
-
-	// void Start () {
-		
-	// }
-	
-	// Update is called once per frame
-	// void Update () {
-		
-	// 	transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
-	// 	var forward = transform.TransformDirection(Vector3.forward);
-	// 	controller.SimpleMove(forward * speed);
-	// }
-
-	// IEnumerator NewHeading ()
-	// {
-	// 	while (true) {
-	// 		NewHeadingRoutine();
-	// 		yield return new WaitForSeconds(directionChangeInterval);
-	// 	}
-	// }
-
-	// void NewHeadingRoutine ()
-	// 	{
-	// 		var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
-	// 		var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
-	// 		heading = Random.Range(floor, ceil);
-	// 		targetRotation = new Vector3(0, heading, 0);
-	// 	}
-	
 }
 
 
