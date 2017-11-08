9c27b01a205d9a63daceaee083255b0184ef6f09
Unknown <bethanylegge19@gmail.com>
2017-11-08 15:28:18 -0700

    added chicken and wolf ai 2. use this one

---
+++ Chicken Game/Assets/Scripts/ChickenAI2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI2 : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public float rotSpeed = 80.0f;
+	public Transform wolf;
+	public Transform player;
+	Vector3 turnAround = new Vector3(0,1,0);
+
+	public Quaternion rot;
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "player"){
+			transform.rotation = new Quaternion(0,180,0,0);
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Wander ();
+	}
+
+
+	void OnTriggerStay2(Collider other){
+		if(other.gameObject.name == "wall"){
+			transform.Rotate(turnAround * rotSpeed * Time.deltaTime);	
+			}
+	}
+	void Wander(){
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Chicken Game/Assets/Scripts/WolfAI2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI2 : MonoBehaviour {
+
+	public float speed=2.0f;
+	public float rotSpeed=20.0f;
+	Vector3 turnAround = new Vector3(0,1,0);
+	public Transform chicken;
+	public Transform player;
+	public float damage;
+
+	void OnTriggerStay(Collider other){
+		
+		if(other.gameObject.tag == "player"){
+			transform.LookAt(player);
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+		if(other.gameObject.tag == "chicken"){
+			transform.LookAt(chicken);
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+	}
+	
+	void Update () {
+	
+		Wander();
+	}
+
+	void Wander(){
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+	
+		
+
+	void OnTriggerStay2(Collider other){
+		if(other.gameObject.tag == "wall"){
+			transform.Rotate(turnAround * rotSpeed * Time.deltaTime);
+	
+		}
+	}
+
+}
