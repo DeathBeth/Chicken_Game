129bc7ab6952c86b2964f3558c84372b17007583
Unknown <bethanylegge19@gmail.com>
2017-12-04 20:18:34 -0700

    played around
    anchored score, added new wintext, added main menu

---
+++ Chicken Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// public void PlayGame(){
+	// 	SceneManager.LoadScene(1);
+	// }
+
+
+	public int levelToLoad;
+
+
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void LevelExit(){
+		Debug.Log("Game Quit");
+		Application.Quit();
+	}
+}
